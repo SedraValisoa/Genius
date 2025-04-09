10ae3b8779fe1389bef4876a32bcd4c8f425827f
sedra <hei.sedra@gmail.com>
2025-04-09 17:40:01 +0300

    [UI] ajout de animation des bouttons

---
+++ src/funtions/anime.js
@@ -0,0 +1,31 @@
+import {
+  createTimeline,
+  stagger,
+  waapi,
+} from "../../node_modules/animejs/lib/anime.esm.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logo = waapi.animate(".anime-logo", {
+    x: "20rem",
+  });
+  const notification = waapi.animate(".anime-notification", {
+    y: "14rem",
+  });
+  const user = waapi.animate(".anime-user", {
+    x: "-18rem",
+  });
+  const footer = waapi.animate(".anime-footer", {
+    x: "-18rem",
+  });
+  const aside = waapi.animate(".anime", {
+    x: "18rem",
+
+    delay: stagger(200),
+  });
+  const tl = createTimeline()
+    .sync(logo, 0)
+    .sync(aside, 120)
+    .sync(user, 200)
+    .sync(notification, 250)
+    .sync(footer, 400);
+});
